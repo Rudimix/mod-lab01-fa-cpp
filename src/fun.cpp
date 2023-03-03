6feeb99b376245772b658c06631b90f8eb7b8888
Rudimix <61971808+Rudimix@users.noreply.github.com>
2023-03-03 19:18:37 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -2,13 +2,98 @@
 #include "fun.h"
 
 unsigned int faStr1(const char *str) {
-    return 0;
+    unsigned int count = 0;
+    bool inWord = false;
+    bool hasDigit = false;
+    while (*str) {
+        if (isalpha(*str)) {
+            inWord = true;
+        }
+        else if (isdigit(*str)) {
+            hasDigit = true;
+        }
+        else if (*str==' ') {
+            if (!hasDigit && inWord) {
+                count++;
+            }
+            inWord = false;
+            hasDigit = false;
+        }
+        str++;
+    }
+    if (*str == '\0') {
+        if (!hasDigit && inWord) {
+            count++;
+        }
+    }
+    return count;
 }
 
 unsigned int faStr2(const char *str) {
-    return 0;
+    unsigned int count = 0;
+    bool inWord = false;
+    bool isCapital = false;
+    bool isRight = false;
+    while (*str) {
+        if (isupper(*str)) {
+            if (inWord && isCapital) {
+                isRight = false;
+                isCapital = false;
+            }
+            else {
+                inWord = true;
+                isCapital = true;
+            }
+        }
+        else if (islower(*str)) {
+            if (inWord && isCapital) {
+                isRight = true;
+            }
+            else isRight = false;
+        }
+        else if (!isupper(*str) && !islower(*str) && isRight) {
+            count++;
+            inWord = false;
+            isCapital = false;
+            isRight = false;
+        }
+        str++;
+    }
+    if (*str == '\0') {
+        if (isRight) {
+            count++;
+        }
+    }
+    return count;
 }
 
 unsigned int faStr3(const char *str) {
-    return 0;
+    unsigned int countWords = 0;
+    unsigned int count = 0;
+    unsigned int SumOfLetters = 0;
+    bool inWord = false;
+    double result;
+    while (*str) {
+        if (*str != ' ') {
+            inWord = true;
+        }
+        if (inWord) {
+            if (*str != ' ') {
+                count++;
+            }
+            else {
+                inWord = false;
+                countWords++;
+                SumOfLetters += count;
+                count = 0;
+            }
+        }
+        str++;
+    }
+    if (*str == '\0') {
+        countWords++;
+        SumOfLetters += count;
+    }
+    result = round(float(SumOfLetters) / countWords);
+    return result;
 }
