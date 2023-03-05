5b0557904f55e24ecf648d369b3bf8e74940c2cc
Rudimix <61971808+Rudimix@users.noreply.github.com>
2023-03-05 17:29:40 +0300

    Update fun.h

--- include/fun.h
+++ include/fun.h
@@ -5,9 +5,5 @@
 unsigned int faStr1(const char *str);
 unsigned int faStr2(const char *str);
 unsigned int faStr3(const char *str);
-islower();
-isupper();
-isalpha();
-isdigit();
 
 #endif  // INCLUDE_FUN_H_
